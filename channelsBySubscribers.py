3523b109743da89bbf1800237854fb96541086e8
Karan Shah <karan.shah05@ad.infosys.com>
2020-09-05 22:59:54 -0400

    New Folder Structure for Youtube API Test Code

--- channelsBySubscribers.py
+++ channelsBySubscribers.py
@@ -8,14 +8,15 @@ import time
 import boto3
 from botocore.exceptions import NoCredentialsError
 
+
 api_service_name = "youtube"
 api_version = "v3"
 youtube_dev_keys = 'AIzaSyAfaRcWzZheVBtQ5o1e3sudkfG9NlNF2js'
 
 youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=youtube_dev_keys)
 
-AWS_ACCESS_KEY_ID = 'AKIAJ4HQ5WOTNEFOBF7A'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'AKIAIKWAPGO3XRCZZM5A'
+AWS_SECRET_ACCESS_KEY = '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'
 bucket_name = 'youtubechannelsdatas3'
 
 def upload_to_s3(data: str, bucket: str, s3_file: str):
@@ -30,26 +31,26 @@ def upload_to_s3(data: str, bucket: str, s3_file: str):
         return False
 
 # Channels Class
-class Channels:
-    def __init__(self, channelid, name):
-        self.channelid= channelid
-        self.name = name
+# class Channels:
+#     def __init__(self, channelid, name):
+#         self.channelid= channelid
+#         self.name = name
 
 # Channel Objects
 
-tseriesObj = Channels("UCq-Fj5jknLsUf-MWSy4_brA",'T-Series')
-pewObj = Channels('UC-lHJZR3Gqxm24_Vd_AJ5Yw','PewDiePie')
-CocomelonObj = Channels('UCbCmjCuTUZos6Inko4u57UQ','Cocomelon - Nursery Rhymes')
-setIndiaObj = Channels('UCpEhnqL0y41EpW2TvWAHD7Q','SET India')
-fiveMinCraftsObj = Channels('UC295-Dw_tDNtZXFeAPAW6Aw', '5-Min Crafts')
-wweObj = Channels('UCJ5v_MCY6GNUBTO8-D3XoAg', 'WWE')
-kidsDianaShowObj = Channels('UCk8GzjMOrta8yxDcKfylJYw','Kids Diana Show')
-zeeMusicObj = Channels('UCFFbwnve3yF62-tVXkTyHqg','Zee Music Company')
-canalKondObj = Channels('UCffDXn7ycAzwL2LDlbyWOTw','Canal KondZilla')
-likeNastyaObj = Channels('UCJplp5SjeGSdVdwsfb9Q7lQ','Like Nastya')
-justinBeiberObj = Channels('UCIwFjwMjI0y7PDBVEO9-bkQ','Justin Bieber')
+# tseriesObj = Channels("UCq-Fj5jknLsUf-MWSy4_brA",'T-Series')
+# pewObj = Channels('UC-lHJZR3Gqxm24_Vd_AJ5Yw','PewDiePie')
+# CocomelonObj = Channels('UCbCmjCuTUZos6Inko4u57UQ','Cocomelon - Nursery Rhymes')
+# setIndiaObj = Channels('UCpEhnqL0y41EpW2TvWAHD7Q','SET India')
+# fiveMinCraftsObj = Channels('UC295-Dw_tDNtZXFeAPAW6Aw', '5-Min Crafts')
+# wweObj = Channels('UCJ5v_MCY6GNUBTO8-D3XoAg', 'WWE')
+# kidsDianaShowObj = Channels('UCk8GzjMOrta8yxDcKfylJYw','Kids Diana Show')
+# zeeMusicObj = Channels('UCFFbwnve3yF62-tVXkTyHqg','Zee Music Company')
+# canalKondObj = Channels('UCffDXn7ycAzwL2LDlbyWOTw','Canal KondZilla')
+# likeNastyaObj = Channels('UCJplp5SjeGSdVdwsfb9Q7lQ','Like Nastya')
+# justinBeiberObj = Channels('UCIwFjwMjI0y7PDBVEO9-bkQ','Justin Bieber')
 
-youtubechannels = [tseriesObj, pewObj, CocomelonObj,setIndiaObj,fiveMinCraftsObj,wweObj,kidsDianaShowObj,zeeMusicObj,canalKondObj,likeNastyaObj,justinBeiberObj]
+# youtubechannels = [tseriesObj, pewObj, CocomelonObj,setIndiaObj,fiveMinCraftsObj,wweObj,kidsDianaShowObj,zeeMusicObj,canalKondObj,likeNastyaObj,justinBeiberObj]
 
 # for channelObj in youtubechannels:
 #    print(channelObj.channelid)
