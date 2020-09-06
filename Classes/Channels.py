c4048e700132a03033fea40de54904506e02479b
Karan Shah <karan.shah05@ad.infosys.com>
2020-09-05 23:02:43 -0400

    File Structure Update

---
+++ Classes/Channels.py
@@ -0,0 +1,5 @@
+class Channels:
+    def __init__(self, channelid, name):
+        self.channelid= channelid
+        self.name = name
+
---
+++ Data/Channels_Data.py
@@ -0,0 +1,15 @@
+from ..Classes import Channels
+
+tseriesObj = Channels("UCq-Fj5jknLsUf-MWSy4_brA",'T-Series')
+pewObj = Channels('UC-lHJZR3Gqxm24_Vd_AJ5Yw','PewDiePie')
+CocomelonObj = Channels('UCbCmjCuTUZos6Inko4u57UQ','Cocomelon - Nursery Rhymes')
+setIndiaObj = Channels('UCpEhnqL0y41EpW2TvWAHD7Q','SET India')
+fiveMinCraftsObj = Channels('UC295-Dw_tDNtZXFeAPAW6Aw', '5-Min Crafts')
+wweObj = Channels('UCJ5v_MCY6GNUBTO8-D3XoAg', 'WWE')
+kidsDianaShowObj = Channels('UCk8GzjMOrta8yxDcKfylJYw','Kids Diana Show')
+zeeMusicObj = Channels('UCFFbwnve3yF62-tVXkTyHqg','Zee Music Company')
+canalKondObj = Channels('UCffDXn7ycAzwL2LDlbyWOTw','Canal KondZilla')
+likeNastyaObj = Channels('UCJplp5SjeGSdVdwsfb9Q7lQ','Like Nastya')
+justinBeiberObj = Channels('UCIwFjwMjI0y7PDBVEO9-bkQ','Justin Bieber')
+
+youtubechannels = [tseriesObj, pewObj, CocomelonObj,setIndiaObj,fiveMinCraftsObj,wweObj,kidsDianaShowObj,zeeMusicObj,canalKondObj,likeNastyaObj,justinBeiberObj]
