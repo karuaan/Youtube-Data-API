f0847aad44c5680751d30c4ca146776654349a4b
Karan Shah <karan.shah05@ad.infosys.com>
2020-09-23 01:38:36 -0400

    Python Duplicate Process Change

--- Backend/channelsBySubscribers.py
+++ Backend/channelsBySubscribers.py
@@ -31,27 +31,27 @@ def upload_to_s3(data: str, bucket: str, s3_file: str):
         print("\nCredentials not available\n")
         return False
 
-# Channels Class
-class Channels:
-    def __init__(self, channelid, name):
-        self.channelid= channelid
-        self.name = name
+# # Channels Class
+# class Channels:
+#     def __init__(self, channelid, name):
+#         self.channelid= channelid
+#         self.name = name
 
-# # Channel Objects
+# # # Channel Objects
 
-tseriesObj = Channels("UCq-Fj5jknLsUf-MWSy4_brA",'T-Series')
-pewObj = Channels('UC-lHJZR3Gqxm24_Vd_AJ5Yw','PewDiePie')
-CocomelonObj = Channels('UCbCmjCuTUZos6Inko4u57UQ','Cocomelon - Nursery Rhymes')
-fiveMinCraftsObj = Channels('UC295-Dw_tDNtZXFeAPAW6Aw', '5-Min Crafts')
-setIndiaObj = Channels('UCpEhnqL0y41EpW2TvWAHD7Q','SET India')
-wweObj = Channels('UCJ5v_MCY6GNUBTO8-D3XoAg', 'WWE')
-kidsDianaShowObj = Channels('UCk8GzjMOrta8yxDcKfylJYw','Kids Diana Show')
-zeeMusicObj = Channels('UCFFbwnve3yF62-tVXkTyHqg','Zee Music Company')
-canalKondObj = Channels('UCffDXn7ycAzwL2LDlbyWOTw','Canal KondZilla')
-likeNastyaObj = Channels('UCJplp5SjeGSdVdwsfb9Q7lQ','Like Nastya')
-justinBeiberObj = Channels('UCIwFjwMjI0y7PDBVEO9-bkQ','Justin Bieber')
+# tseriesObj = Channels("UCq-Fj5jknLsUf-MWSy4_brA",'T-Series')
+# pewObj = Channels('UC-lHJZR3Gqxm24_Vd_AJ5Yw','PewDiePie')
+# CocomelonObj = Channels('UCbCmjCuTUZos6Inko4u57UQ','Cocomelon - Nursery Rhymes')
+# fiveMinCraftsObj = Channels('UC295-Dw_tDNtZXFeAPAW6Aw', '5-Min Crafts')
+# setIndiaObj = Channels('UCpEhnqL0y41EpW2TvWAHD7Q','SET India')
+# wweObj = Channels('UCJ5v_MCY6GNUBTO8-D3XoAg', 'WWE')
+# kidsDianaShowObj = Channels('UCk8GzjMOrta8yxDcKfylJYw','Kids Diana Show')
+# zeeMusicObj = Channels('UCFFbwnve3yF62-tVXkTyHqg','Zee Music Company')
+# canalKondObj = Channels('UCffDXn7ycAzwL2LDlbyWOTw','Canal KondZilla')
+# likeNastyaObj = Channels('UCJplp5SjeGSdVdwsfb9Q7lQ','Like Nastya')
+# justinBeiberObj = Channels('UCIwFjwMjI0y7PDBVEO9-bkQ','Justin Bieber')
 
-youtubechannels = [tseriesObj, pewObj, CocomelonObj,setIndiaObj,fiveMinCraftsObj,wweObj,kidsDianaShowObj,zeeMusicObj,canalKondObj,likeNastyaObj,justinBeiberObj]
+# youtubechannels = [tseriesObj, pewObj, CocomelonObj,setIndiaObj,fiveMinCraftsObj,wweObj,kidsDianaShowObj,zeeMusicObj,canalKondObj,likeNastyaObj,justinBeiberObj]
 
 # for channelObj in youtubechannels:
 #    print(channelObj.channelid)
