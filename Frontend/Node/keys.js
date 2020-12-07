7fcbf7516698f26dee2a079e74dba953ec0bbf26
karuaan <karuaan@gmail.com>
2020-12-07 18:18:46 -0500

    Update keys.js

--- Frontend/Node/keys.js
+++ Frontend/Node/keys.js
@@ -1,11 +1,11 @@
 const AWS_Keys = {
-  accessKeyId: "AKIAJIW2J6HTZTLMBJYA",
-  secretAccessKey: "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",
-  bucketName: "youtubechannelsdatas3",
-  readFileName: "latest.json",
-  mychannelsfile: "mychannelsdata.json",
-  channelsListFile: "myChannels.json",
-  Region: "us-east-2"
+  accessKeyId: "XXXXXXXXXXXXXXXXX",
+  secretAccessKey: "XXXXXXXXXXXXXXX",
+  bucketName: "XXXXXXXXXXXXXXXX",
+  readFileName: "XXXXXXXXXXXXXX",
+  mychannelsfile: "XXXXXXXXXXXXX",
+  channelsListFile: "XXXXXXXXXXXXXX",
+  Region: "XXXXXXXXXXXXXXX"
 };
 
 module.exports.AWS_Keys = AWS_Keys;
