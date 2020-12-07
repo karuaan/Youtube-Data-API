bc730260b40cb460331e3240094a6b9723c2719b
Karan Shah <karan@Karans-MBP.fios-router.home>
2020-12-07 18:02:38 -0500

    Added Keys files for better visibility.

---
+++ Frontend/client/Keys.js
@@ -0,0 +1,11 @@
+const AWS_Keys = {
+    accessKeyId: "XXXXXXXXXXX",
+    secretAccessKey: "XXXXXXXXXXX",
+    bucketName: "XXXXXXXXXXX",
+    readFileName: "XXXXXXXXXXX",
+    mychannelsfile: "XXXXXXXXXXX",
+    channelsListFile: "XXXXXXXXXXX",
+    Region: "XXXXXXXXXXX"
+  };
+  
+module.exports.AWS_Keys = AWS_Keys;
