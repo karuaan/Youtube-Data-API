3717a1891017f8b29a644ee8a4f90215045d6b29
Karan Shah <karan@Karans-MBP.fios-router.home>
2020-12-07 20:54:57 -0500

    Added Keys file to Frontend Node Folder

---
+++ Frontend/Node/Keys.js
@@ -0,0 +1,11 @@
+const AWS_Keys = {
+    accessKeyId: "Place AWS Access Key Here",
+    secretAccessKey: "Place AWS Secret Key Here",
+    bucketName: "Place your bucket name here",
+    readFileName: "latest.json",
+    mychannelsfile: "mychannelsdata.json",
+    channelsListFile: "myChannels.json",
+    Region: "us-east-2"
+  };
+  
+module.exports.AWS_Keys = AWS_Keys;
